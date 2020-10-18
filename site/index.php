b0cf0c394a36444b93021c02ed38f0621dcdeb2f
Harry French <harry.french@moonforge.com>
2020-10-18 01:19:40 +0100

    bits and pieces

--- site/index.php
+++ site/index.php
@@ -2,6 +2,12 @@
   <head>
   </head>
 <style>
+
+@font-face {
+  font-family: 'Quentin';
+  src: url('./fonts/quentin.otf');
+}
+
 body {
   background-image: url("img/oo_logo.svg");
   background-color: #cccccc;
@@ -11,15 +17,20 @@ body {
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
-font-family:sans-serif;
+  font-family:sans-serif;
 }
-<style>
+
+.curly {
+  font-family:'Quentin';
+  font-size:2em;
+}
+
 /* customizable snowflake styling */
 .snowflake {
-  color: #ffffff;
+  color: #C005AF;
   font-size: 1em;
   font-family: Arial, sans-serif;
-  text-shadow: 0 0 5px #000;
+  text-shadow: 0 0 5px #fff;
 }
 
 @-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}
@@ -62,15 +73,15 @@ font-family:sans-serif;
     ♡
   </div>
 </div>
-</style>
 <body>
 <div>We are a brand new wedding services company based in Madley, Telford.</div>
-<ul>
-<li>Wedding Dressing</li>
-<li>Wedding Dresses</li>
-<li>Flowergirl Dresses</li>
-<li>Prom Dresses</li>
-</ul>
+
+<div style="position:relative; height
+
+<div class="curly">Wedding Dressing</div>
+<div class="curly">Wedding Dresses</div>
+<div class="curly">Flowergirl Dresses</div>
+<div class="curly">Prom Dresses</div>
 <div>Contact <a href="mailto:nicola@opulent-occasions.co.uk">Nicola</a> for more information</div>
 <form>
 </form>
