09badac2e41c43ba28c4ef1b2e16893f06f605fd
Harry French <harry.french@moonforge.com>
2020-10-22 23:30:34 +0100

    some changes

--- site/index.php
+++ site/index.php
@@ -1,88 +1,189 @@
 <html>
-  <head>
-  </head>
-<style>
+  <head> </head>
+  <style>
+    @font-face {
+      font-family: 'Quentin';
+      src: url('./fonts/quentin.otf');
+    }
 
-@font-face {
-  font-family: 'Quentin';
-  src: url('./fonts/quentin.otf');
-}
+    body {
+      background-image: url('img/oo_logo.svg');
+      background-color: #cccccc;
+      width: 100%;
 
-body {
-  background-image: url("img/oo_logo.svg");
-  background-color: #cccccc;
-  height: 100%;
+      /* Center and scale the image nicely */
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: contain;
+      font-family: sans-serif;
+    }
 
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  font-family:sans-serif;
-}
+    .curly {
+      font-family: 'Quentin';
+      font-size: 2.25em;
+      color: #c005af;
+      color: #c00268;
+    }
 
-.curly {
-  font-family:'Quentin';
-  font-size:2.25em;
-  color:#C005AF;
-  color: #C00268;
-}
+    /* customizable snowflake styling */
+    .snowflake {
+      color: #c00268;
+      font-size: 2em;
+      font-family: Arial, sans-serif;
+      text-shadow: 0 0 5px #fff;
+    }
 
-/* customizable snowflake styling */
-.snowflake {
-  color: #C005AF;
-  font-size: 1em;
-  font-family: Arial, sans-serif;
-  text-shadow: 0 0 5px #fff;
-}
-
-@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}
-</style>
-<div class="snowflakes" aria-hidden="true">
-  <div class="snowflake">
-  ♥
+    @-webkit-keyframes snowflakes-fall {
+      0% {
+        top: -10%;
+      }
+      100% {
+        top: 100%;
+      }
+    }
+    @-webkit-keyframes snowflakes-shake {
+      0%,
+      100% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0);
+      }
+      50% {
+        -webkit-transform: translateX(80px);
+        transform: translateX(80px);
+      }
+    }
+    @keyframes snowflakes-fall {
+      0% {
+        top: -10%;
+      }
+      100% {
+        top: 100%;
+      }
+    }
+    @keyframes snowflakes-shake {
+      0%,
+      100% {
+        transform: translateX(0);
+      }
+      50% {
+        transform: translateX(80px);
+      }
+    }
+    .snowflake {
+      position: fixed;
+      top: -10%;
+      z-index: 9999;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      cursor: default;
+      -webkit-animation-name: snowflakes-fall, snowflakes-shake;
+      -webkit-animation-duration: 10s, 3s;
+      -webkit-animation-timing-function: linear, ease-in-out;
+      -webkit-animation-iteration-count: infinite, infinite;
+      -webkit-animation-play-state: running, running;
+      animation-name: snowflakes-fall, snowflakes-shake;
+      animation-duration: 10s, 3s;
+      animation-timing-function: linear, ease-in-out;
+      animation-iteration-count: infinite, infinite;
+      animation-play-state: running, running;
+    }
+    .snowflake:nth-of-type(0) {
+      left: 1%;
+      -webkit-animation-delay: 0s, 0s;
+      animation-delay: 0s, 0s;
+    }
+    .snowflake:nth-of-type(1) {
+      left: 10%;
+      -webkit-animation-delay: 1s, 1s;
+      animation-delay: 1s, 1s;
+    }
+    .snowflake:nth-of-type(2) {
+      left: 20%;
+      -webkit-animation-delay: 6s, 0.5s;
+      animation-delay: 6s, 0.5s;
+    }
+    .snowflake:nth-of-type(3) {
+      left: 30%;
+      -webkit-animation-delay: 4s, 2s;
+      animation-delay: 4s, 2s;
+    }
+    .snowflake:nth-of-type(4) {
+      left: 40%;
+      -webkit-animation-delay: 2s, 2s;
+      animation-delay: 2s, 2s;
+    }
+    .snowflake:nth-of-type(5) {
+      left: 50%;
+      -webkit-animation-delay: 8s, 3s;
+      animation-delay: 8s, 3s;
+    }
+    .snowflake:nth-of-type(6) {
+      left: 60%;
+      -webkit-animation-delay: 6s, 2s;
+      animation-delay: 6s, 2s;
+    }
+    .snowflake:nth-of-type(7) {
+      left: 70%;
+      -webkit-animation-delay: 2.5s, 1s;
+      animation-delay: 2.5s, 1s;
+    }
+    .snowflake:nth-of-type(8) {
+      left: 80%;
+      -webkit-animation-delay: 1s, 0s;
+      animation-delay: 1s, 0s;
+    }
+    .snowflake:nth-of-type(9) {
+      left: 90%;
+      -webkit-animation-delay: 3s, 1.5s;
+      animation-delay: 3s, 1.5s;
+    }
+    .snowflake:nth-of-type(10) {
+      left: 25%;
+      -webkit-animation-delay: 2s, 0s;
+      animation-delay: 2s, 0s;
+    }
+    .snowflake:nth-of-type(11) {
+      left: 65%;
+      -webkit-animation-delay: 4s, 2.5s;
+      animation-delay: 4s, 2.5s;
+    }
+  </style>
+  <div class="snowflakes" aria-hidden="true">
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
+    <div class="snowflake">♥</div>
+    <div class="snowflake">♡</div>
   </div>
-  <div class="snowflake">
-  ♡
-  </div>
-  <div class="snowflake">
-  ♥
-  </div>
-  <div class="snowflake">
-  ♡
-  </div>
-  <div class="snowflake">
-  ♥
-  </div>
-  <div class="snowflake">
-  ♡
-  </div>
-  <div class="snowflake">
-  ♥
-  </div>
-  <div class="snowflake">
-    ♡
-  </div>
-  <div class="snowflake">
-    ♥
-  </div>
-  <div class="snowflake">
-    ♡
-  </div>
-  <div class="snowflake">
-    ♥
-  </div>
-  <div class="snowflake">
-    ♡
-  </div>
-</div>
-<body>
-<div>We are a brand new wedding services company based in Madley, Telford.</div>
-<div class="curly" style="position:absolute; left:0.5em; top:14em;">Wedding Dressing</div>
-<div class="curly" style="position:absolute; left:1.5em; top:16em;">Wedding Dresses</div>
-<div class="curly" style="position:absolute; left:2.5em; top:18em;">Flowergirl Dresses</div>
-<div class="curly" style="position:absolute; left:3.5em; top:20em;">Prom Dresses</div>
-<div>Contact <a href="mailto:nicola@opulent-occasions.co.uk">Nicola</a> for more information</div>
-<form>
-</form>
-</body>
+  <body>
+    <div class="curly" style="text-align: right; position: absolute; right: 5%; top: 75%">
+      Wedding Dressing
+    </div>
+    <div class="curly" style="text-align: right; position: absolute; left: 5%; top: 50%">
+      Wedding<br/>Dresses
+    </div>
+    <div class="curly" style="text-align: right; position: absolute; left: 15%; top: 70%">
+      Flowergirl<br/>Dresses
+    </div>
+    <div class="curly" style="text-align: right; position: absolute; right: 10%; top: 50%">
+      Prom<br/>Dresses
+    </div>
+    <div>
+      Contact <a href="mailto:nicola@opulent-occasions.co.uk">Nicola</a> for
+      more information
+    </div>
+    <form></form>
+    <div>
+      We are a brand new wedding services company based in Madley, Telford.
+    </div>
+  </body>
 </html>
