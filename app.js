d68f9e988f988446c84cc0eedc17ae14ad81f0fc
Harry French <harry.french@moonforge.com>
2021-05-10 22:33:10 +0100

    port fixed

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on ${process.env.PORT || 3000}`);
 })
 
 module.exports = app;
